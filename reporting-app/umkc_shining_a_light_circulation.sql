29c3f52e5e9f49f12809d93526abd32fa53029f6
Buddy Pennington <penningtonb@umkc.edu>
2025-02-20 14:14:06 -0600

    Adding in loans and renewal count data

--- reporting-app/umkc_shining_a_light_circulation.sql
+++ reporting-app/umkc_shining_a_light_circulation.sql
@@ -17,7 +17,9 @@ RETURNS TABLE(
     instance_id text,
     status_name text,
     created_date text,
-    statistical_code_name text)
+    statistical_code_name text,
+    total_loans text,
+    total_renewals text)
 AS $$
 SELECT
 	ll.campus_name as campus_name,
@@ -30,14 +32,17 @@ SELECT
 	instext.instance_id as instance_id,
 	itemext.status_name as status_name,
 	itemext.created_date as created_date,
-	stat.statistical_code_name as statistical_code_name 
+	stat.statistical_code_name as statistical_code_name,
+    circ.num_loans,
+	circ.num_renewals  
 FROM folio_derived.instance_ext as instext
      LEFT JOIN folio_derived.holdings_ext AS he ON instext.instance_id = he.instance_id        
      LEFT JOIN folio_derived.locations_libraries AS ll ON he.permanent_location_id = ll.location_id       
      LEFT JOIN folio_derived.item_ext AS itemext ON he.holdings_id = itemext.holdings_record_id
      LEFT JOIN folio_inventory.item__t AS ii ON itemext.item_id = ii.id
-     left join folio_derived.instance_formats as instfmt on instext.instance_id = instfmt.instance_id
-     left join folio_derived.item_statistical_codes as stat on itemext.item_id = stat.item_id
+     LEFT JOIN folio_derived.instance_formats as instfmt on instext.instance_id = instfmt.instance_id
+     LEFT JOIN folio_derived.item_statistical_codes as stat on itemext.item_id = stat.item_id
+     LEFT JOIN folio_derived.loans_renewal_count as circ on itemext.item_id = circ.item_id
 WHERE (ll.library_name='UMKC Music/Media Library')
 	AND stat.statistical_code = 'umkcshining'
 ORDER BY he.permanent_location_name 
