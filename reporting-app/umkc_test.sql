132dafe0c86c3483cf145280a8eb02bc93f12a75
Buddy Pennington <penningtonb@umkc.edu>
2025-03-18 15:01:09 -0500

    Update umkc_test.sql

--- reporting-app/umkc_test.sql
+++ reporting-app/umkc_test.sql
@@ -7,14 +7,19 @@ DROP FUNCTION IF EXISTS umkc_test;
 CREATE FUNCTION umkc_test()
 RETURNS TABLE(
     item_id text,
+    barcode text,
+    item_location text,
     loans bigint,
     renewals bigint)
 AS $$
 SELECT 
 	circ.item_id as item_id,
+    items.barcode as barcode,
+    items.effective_location_name as item_location,
 	circ.num_loans as loans,
 	circ.num_renewals as renewals
 from folio_derived.loans_renewal_count as circ
+left join folio_derived.item_ext as items on circ.item_id = items.item_id
 where circ.num_loans > 0
 $$
 LANGUAGE SQL
