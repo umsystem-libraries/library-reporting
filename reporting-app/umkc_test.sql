7a790538ecb263fef1f0e73b0b94954434789835
Buddy Pennington <penningtonb@umkc.edu>
2025-03-18 15:27:09 -0500

    Update umkc_test.sql

--- reporting-app/umkc_test.sql
+++ reporting-app/umkc_test.sql
@@ -7,7 +7,7 @@ DROP FUNCTION IF EXISTS umkc_test;
 CREATE FUNCTION umkc_test()
 RETURNS TABLE(
     statistical_code text,
-    item_id text,
+    title text,
     barcode text,
     item_location text,
     loans bigint,
@@ -15,7 +15,7 @@ RETURNS TABLE(
 AS $$
 SELECT 
 	stat.statistical_code_name as statistical_code,
-    circ.item_id as item_id,
+    instances.title as title,
     items.barcode as barcode,
     items.effective_location_name as item_location,
 	circ.num_loans as loans,
@@ -23,6 +23,8 @@ SELECT
 from folio_derived.loans_renewal_count as circ
     left join folio_derived.item_ext as items on circ.item_id = items.item_id
     left join folio_derived.item_statistical_codes as stat on items.item_id = stat.item_id
+    LEFT JOIN folio_derived.holdings_ext AS holdings ON holdings.holdings_id = items.holdings_record_id
+	left join folio_derived.instance_ext as instances on holdings.instance_id = instances.instance_id
 where circ.num_loans > 0 and stat.statistical_code = 'umkcshining'
 $$
 LANGUAGE SQL
