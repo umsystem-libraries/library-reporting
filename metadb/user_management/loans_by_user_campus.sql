e0c82ed51beb5dc7da6f0b7772330614248cbd13
Steven Pryor <31045615+spryor2@users.noreply.github.com>
2025-04-04 16:41:23 -0500

    Add user campus, barcode, and 'aged to lost' criteria

--- metadb/user_management/loans_by_user_campus.sql
+++ metadb/user_management/loans_by_user_campus.sql
@@ -1,6 +1,9 @@
 select 
+	u.jsonb -> 'customFields' ->> 'campus_2' as user_campus,
+	u.jsonb ->> 'barcode' as user_barcode,
 	li.current_item_permanent_location_campus_name,
 	li.barcode,
+	li.item_status,
 	instext.title,
 	u.jsonb	
 from folio_derived.loans_items as li
@@ -9,4 +12,5 @@ from folio_derived.loans_items as li
 	left join folio_derived.instance_ext as instext on he.instance_id = instext.instance_id
 where u.jsonb @> '{"customFields":{"campus_2":["opt_3"]}}' and "__current" = true
 	and li.loan_status = 'Open'
+	and li.item_status = 'Aged to lost'
 ;
