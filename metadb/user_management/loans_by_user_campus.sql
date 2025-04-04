f5acb865fee2d8ea69f7433b60a57aaeb7a96f6c
Steven Pryor <31045615+spryor2@users.noreply.github.com>
2025-04-04 16:22:32 -0500

    Link up holding and instance tables

--- metadb/user_management/loans_by_user_campus.sql
+++ metadb/user_management/loans_by_user_campus.sql
@@ -1,11 +1,12 @@
 select 
-	li.barcode,
 	li.current_item_permanent_location_campus_name,
-	li.user_id,
-	u.id,
+	li.barcode,
+	instext.title,
 	u.jsonb	
 from folio_derived.loans_items as li
 	left join folio_users.users__ as u on li.user_id = u.id
+	left join folio_derived.holdings_ext AS he ON li.holdings_record_id = he.holdings_id
+	left join folio_derived.instance_ext as instext on he.instance_id = instext.instance_id
 where u.jsonb @> '{"customFields":{"campus_2":["opt_3"]}}' and "__current" = true
 	and li.loan_status = 'Open'
 ;
