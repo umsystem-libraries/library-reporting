4e6aaaec8d07e9ffa0a3a9e52fa7bc044834f951
Buddy Pennington <penningtonb@umkc.edu>
2025-06-24 12:54:22 -0500

    MOBIUS Lending Counts for Reporting App

---
+++ reporting-app/MOBIUS_lending_counts.sql
@@ -0,0 +1,23 @@
+--metadb:function mobius_loans
+
+DROP FUNCTION IF EXISTS mobius_loans;
+
+CREATE FUNCTION mobius_loans(
+    start_date date DEFAULT '1000-01-01',
+    end_date date DEFAULT '3000-01-01'),
+    campus text DEFAULT 'University of Missouri - Kansas City'
+
+RETURNS TABLE(
+    library_name text,
+    loan_count bigint)
+AS $$
+SELECT ,
+	l.current_item_permanent_location_library_name,	
+	count (l.loan_id)
+    FROM folio_derived.loans_items AS l
+    WHERE l.loan_date between start_date and end_date and and l.current_item_permanent_location_campus_name = campus and l.checkout_service_point_name like 'DCB%'
+    GROUP BY l.current_item_permanent_location_library_name
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;
