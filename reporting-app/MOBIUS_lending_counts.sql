e2abc0cf9c3fb45c65796f091642421e89d3c43c
Buddy Pennington <penningtonb@umkc.edu>
2025-06-30 15:11:07 -0500

    Adding date inputs

--- reporting-app/MOBIUS_lending_counts.sql
+++ reporting-app/MOBIUS_lending_counts.sql
@@ -3,6 +3,8 @@
 DROP FUNCTION IF EXISTS mobius_loans;
 
 CREATE FUNCTION mobius_loans(
+    start_date date DEFAULT '2000-01-01',
+    end_date date DEFAULT '2050-01-01',
 )
 RETURNS TABLE(
     library_name text,
@@ -13,7 +15,7 @@ SELECT
 	l.current_item_permanent_location_library_name as library_name,
     loan_id as loan_count
     FROM folio_derived.loans_items AS l
-    WHERE l.loan_date between '2025-01-01' and '2025-07-01' and l.current_item_permanent_location_campus_name = 'University of Missouri - Kansas City' and l.checkout_service_point_name like 'DCB%'
+    WHERE (l.loan_date between start_date and end_date) and l.current_item_permanent_location_campus_name = 'University of Missouri - Kansas City' and l.checkout_service_point_name like 'DCB%'
     GROUP BY l.current_item_permanent_location_library_name
 $$
 LANGUAGE SQL
