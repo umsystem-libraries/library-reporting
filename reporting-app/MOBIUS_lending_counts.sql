1c32448ac73849be6a0434e2f7e61d5a631ade72
Buddy Pennington <penningtonb@umkc.edu>
2025-06-30 14:59:22 -0500

    Update MOBIUS_lending_counts.sql

--- reporting-app/MOBIUS_lending_counts.sql
+++ reporting-app/MOBIUS_lending_counts.sql
@@ -5,11 +5,13 @@ DROP FUNCTION IF EXISTS mobius_loans;
 CREATE FUNCTION mobius_loans(
 )
 RETURNS TABLE(
-    library_name text
+    library_name text,
+    loan_count bigint
 )
 AS $$
 SELECT
-	l.current_item_permanent_location_library_name as library_name
+	l.current_item_permanent_location_library_name as library_name,
+    count (*) as loan_count
     FROM folio_derived.loans_items AS l
     WHERE l.loan_date between '2024-07-01' and '2025-07-01' and l.current_item_permanent_location_campus_name = 'University of Missouri - Kansas City' and l.checkout_service_point_name like 'DCB%'
     GROUP BY l.current_item_permanent_location_library_name
