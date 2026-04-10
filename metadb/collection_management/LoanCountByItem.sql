c0482f355f982d0e6572e5b79169026626546547
Steven Pryor <31045615+spryor2@users.noreply.github.com>
2026-04-10 15:47:27 -0500

    Add last loan & return dates, move date condition to JOIN...
    ...so that all items, even ones with no loans (and thus no loan date at all), are returned.

--- metadb/collection_management/LoanCountByItem.sql
+++ metadb/collection_management/LoanCountByItem.sql
@@ -5,14 +5,18 @@ SELECT
 	ie.item_hrid,
 	ie.barcode,
 	ie.permanent_location_name,
-	COUNT(li.loan_id) as "Number of Loans"
+	li.loan_policy_name,
+	COUNT(li.loan_id) as "Number of Loans",
+	coalesce(SUM(li.renewal_count), 0) as "Number of Renewals",
+	MAX(li.loan_date) AS "Last Loan Date",
+	MAX(li.loan_return_date) as "Last Return Date",
+	ie.material_type_name as "material type"
 --	li.loan_date,
 --	li.loan_status
 from folio_derived.instance_ext as instext
     LEFT JOIN folio_derived.holdings_ext AS he ON instext.instance_id = he.instance_id
     LEFT JOIN folio_derived.item_ext AS ie ON he.holdings_id = ie.holdings_record_id     
-	LEFT JOIN folio_derived.loans_items AS li ON ie.item_id = li.item_id
+	LEFT JOIN folio_derived.loans_items AS li ON ie.item_id = li.item_id and li.loan_date > DATE '2023-01-01'
 	left join folio_derived.locations_libraries as ll on ie.permanent_location_id = ll.location_id 
-WHERE li.loan_date > '05/01/2020'
-	and ll.library_code = 'CENGR'
-group by instext.instance_hrid, instext.title, ie.effective_call_number, ie.item_hrid, ie.barcode, ie.permanent_location_name 
+where ll.library_code = 'CENGR' 
+group by instext.instance_hrid, instext.title, ie.effective_call_number, ie.item_hrid, ie.barcode, ie.permanent_location_name, li.loan_policy_name, ie.material_type_name  
