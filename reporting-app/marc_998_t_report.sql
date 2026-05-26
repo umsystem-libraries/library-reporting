9207766718ed124b223b72d942b270dfa9c84afb
Buddy Pennington <penningtonb@umkc.edu>
2026-05-26 16:09:15 -0500

    Update marc_998_t_report.sql

--- reporting-app/marc_998_t_report.sql
+++ reporting-app/marc_998_t_report.sql
@@ -1,6 +1,18 @@
-CREATE FUNCTION test_report()
-RETURNS TABLE(x text)
-LANGUAGE sql
+CREATE OR REPLACE FUNCTION marc_mobius()
+RETURNS TABLE(
+    hrid text,
+    title text,
+    marc_998t text
+)
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE
 AS $$
-SELECT 'ok'
+SELECT 
+    i.instance_hrid as hrid,
+    i.title as title,
+    mt.content as marc_998t
+from folio_source_record.marc__t mt
+JOIN folio_derived.instance_ext i ON i.instance_id = mt.instance_id
+WHERE mt.field = '998' AND mt.sf = 't' AND mt.content is not null
 $$
