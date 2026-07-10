22cc12f7287eebd569014d65f7992a6b8bf7a569
krahman-um <krahman@missouri.edu>
2026-07-10 14:13:50 -0500

    Update InHouseUseByLibrary.sql
    Fixed holding uuid column name in derived table holdings_ext

--- metadb/collection_management/InHouseUseByLibrary.sql
+++ metadb/collection_management/InHouseUseByLibrary.sql
@@ -8,7 +8,7 @@ select
 	COUNT(ci.id) as "In House Uses"
 from folio_derived.instance_ext as instext
     LEFT JOIN folio_derived.holdings_ext AS he ON instext.instance_id = he.instance_id
-    LEFT JOIN folio_derived.item_ext AS ie ON he.holdings_id = ie.holdings_record_id     
+    LEFT JOIN folio_derived.item_ext AS ie ON he.id = ie.holdings_record_id     
 	LEFT JOIN folio_derived.loans_items AS li ON ie.item_id = li.item_id
 	left join folio_derived.locations_libraries as ll on ie.permanent_location_id = ll.location_id 
 	LEFT JOIN folio_circulation.check_in__t__ AS ci ON ie.item_id = ci.item_id
